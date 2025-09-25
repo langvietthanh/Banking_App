f97fc935af18dcb82fc9f26ef5cefec92f319c6a
lang thanh <langvietthanh.28072005@gmail.com>
2025-09-25 21:20:51 +0700

    Kết nối với database 'BankingApp thành công'

---
+++ App/src/JDBC/JDBCUtil.java
@@ -0,0 +1,34 @@
+package JDBC;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class JDBCUtil {
+	private static final String URL = "jdbc:mariadb://localhost:3306/BankingApp",
+	USER = "root",
+	PASSWORD = "";
+	
+	 public static Connection getConnection() {
+	        Connection conn = null;
+	        try {
+	            // Từ JDBC 4.0 trở đi không cần Class.forName nữa, chỉ cần có driver trong classpath
+	            conn = DriverManager.getConnection(URL, USER, PASSWORD);
+	            System.out.println("Kết nối MySQL thành công!");
+	        } catch (SQLException e) {
+	            e.printStackTrace();
+	        }
+	        return conn;
+	    }
+
+	    public static void disconnect(Connection conn) {
+	        if (conn != null) {
+	            try {
+	                conn.close();
+	                System.out.println("Đã đóng kết nối.");
+	            } catch (SQLException e) {
+	                e.printStackTrace();
+	            }
+	        }
+	    }
+}
