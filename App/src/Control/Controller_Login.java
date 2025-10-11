3b32abd60e1236c7cf6734bc08a2f95ee620e6c5
lang thanh <langvietthanh.28072005@gmail.com>
2025-10-11 22:56:42 +0700

    Kiểm tra SDT khi người dùng nhập từ giao diện

--- App/src/Control/Controller_Login.java
+++ App/src/Control/Controller_Login.java
@@ -13,33 +13,37 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.EventObject;
 import java.util.Objects;
 
 public class Controller_Login {
     @FXML
-    private TextField usernameField;
+    private TextField TextField_SoDienThoai;
     private static int LuotDangNhapSai = 0;
 
     @FXML
-    private PasswordField passwordField;
+    private PasswordField TextField_Password;
 
-    public void handleLogin(ActionEvent actionEvent ) throws IOException {
-        String sdt = usernameField.getText();
-        String password = passwordField.getText();
+    public void handleLogin(ActionEvent actionEvent ) throws IOException, SQLException {
+        String sdt = TextField_SoDienThoai.getText();
+        String password = TextField_Password.getText();
         NguoiDungDAO ndd = new NguoiDungDAO();
-        if (! ndd.Ton_Tai_Nguoi_Dung(sdt)){
-//            popup tbao
-        }
+
+        if(sdt.isEmpty()) Xuat_Thong_Bao_SDT_Empty();
+        else if(!NguoiDung.Kiem_Tra_SDT(sdt)) Xuat_Thong_Bao_SDT_Sai_Dinh_Dang();
+        else if (!ndd.Ton_Tai_Nguoi_Dung(sdt)) Xuat_Thong_Bao_Loi();
         else {
+//           Người dùng tồn tại
             NguoiDung nd = ndd.Dang_Nhap(sdt, password);
             //      Đăng nhập thành công => nd tồn tại
-            if (nd != null && LuotDangNhapSai < 3) {
+            if (nd != null && LuotDangNhapSai < 3 ) {
                 Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/View/DashBoard.fxml")));
                 Scene scene = new Scene(root);
 
@@ -48,12 +52,35 @@ public class Controller_Login {
                 stage.setTitle("Banking App");
                 stage.show();
             }
-            //        Đăng nhập sai tôi đa 3 lần
+            //        Đăng nhập sai tôi đa 3 lần => giảm 1 mức uy tín
             else {
                 LuotDangNhapSai++;
-                System.out.print("wrong" + LuotDangNhapSai);
+
             }
         }
+        TextField_SoDienThoai.setText("");
+        TextField_Password.setText("");
+    }
+
+    private void Xuat_Thong_Bao_SDT_Sai_Dinh_Dang() {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Lỗi đăng nhập!");
+        alert.setContentText("Định dạng số điện không phù hợp");
+        alert.showAndWait();
+    }
+
+    private void Xuat_Thong_Bao_SDT_Empty() {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Lỗi đăng nhập!");
+        alert.setContentText("Vui lòng nhập số điện thoại!");
+        alert.showAndWait();
+    }
+
+    private void Xuat_Thong_Bao_Loi() {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Lỗi đăng nhập!");
+        alert.setContentText("Người dùng chưa tồn tại trong hệ thống");
+        alert.showAndWait();
     }
 
     public void handleRegister(ActionEvent actionEvent) throws IOException {
